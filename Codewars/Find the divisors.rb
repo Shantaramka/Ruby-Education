d1ef1d3d043eae9400a1b66681617ca2f9e414ec
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2019-12-27 15:55:07 +0300

    Create Find the divisors.rb

---
+++ Codewars/Find the divisors.rb
@@ -0,0 +1,25 @@
+=begin
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12) # should return [2,3,4,6]
+divisors(25) # should return [5]
+divisors(13) # should return "13 is prime"
+=end
+
+
+def divisors(n)
+  $res = []
+  
+  2.upto(n-1) do |div|
+    if n % div == 0   
+      $res.push(div)
+    end
+  end
+
+  if $res == []
+    return "#{n} is prime"
+  end
+
+  return $res
+end
